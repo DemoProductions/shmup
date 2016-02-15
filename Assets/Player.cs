0ec516312666bc6275cb51b37ba54560aeb9763e
flip40 <john@ludeman.com>
2016-02-14 22:41:57 -0800

    added initial shooting

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,12 +8,18 @@ public class Player : MonoBehaviour {
 	public float yvelocity;
 	public Vector2 movement;
 
+	public GameObject bullet;
+	public float bullettime;
+	public const float BULLET_DELAY = 0.1f;
+
 	private Rigidbody2D rbody;
 
 	// Use this for initialization
 	void Start () {
 		xvelocity = 0;
 		yvelocity = 0;
+
+		bullettime = 0;
 
 		rbody = gameObject.GetComponent<Rigidbody2D> ();
 	}
@@ -22,6 +28,14 @@ public class Player : MonoBehaviour {
 	void Update () {
 		xvelocity = Input.GetAxis ("Horizontal");
 		yvelocity = Input.GetAxis ("Vertical");
+
+		bullettime += Time.deltaTime;
+
+		if (Input.GetAxis ("Fire1") > 0 && bullettime >= BULLET_DELAY) {
+			//I would argue we need a better way to store the bullet prefab, allowing upgrades and such to change this.
+			Instantiate (this.bullet, this.transform.position, this.transform.rotation * Quaternion.Euler(0, 0, 90));
+			bullettime = 0;
+		}
 	}
 
 	void FixedUpdate() {
