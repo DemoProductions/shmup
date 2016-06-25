fe8cd190179e95719ffe5e2616515138cf8b66a2
Adrian Lin <adrianylin@gmail.com>
2016-06-25 13:54:46 -0700

    initial weapon script
    tab issue
    
    direction of bullet in weapon class for now
    
    handle different bullet types
    
    bullettime and bulletdelay in weapon

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,43 +3,36 @@
 public class Player : MonoBehaviour
 {
 
-	public int speed = 100;
-	public float xvelocity;
-	public float yvelocity;
-
-	public GameObject bullet;
-	public float bullettime;
-	public const float BULLET_DELAY = 0.1f;
+    public int speed = 100;
+    public float xvelocity;
+    public float yvelocity;
 
     Health health;
+    Weapon weapon;
+    Rigidbody2D rbody;
 
-	Rigidbody2D rbody;
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         health = gameObject.GetComponent<Health> ();
-		rbody = gameObject.GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        weapon = gameObject.GetComponent<Weapon> ();
+        rbody = gameObject.GetComponent<Rigidbody2D> ();
+    }
+    
+    // Update is called once per frame
+    void Update ()
     {
-		xvelocity = Input.GetAxis ("Horizontal");
-		yvelocity = Input.GetAxis ("Vertical");
+        xvelocity = Input.GetAxis ("Horizontal");
+        yvelocity = Input.GetAxis ("Vertical");
 
-		if (bullettime < BULLET_DELAY) bullettime += Time.deltaTime;
-
-		if (Input.GetAxis ("Fire1") > 0 && bullettime >= BULLET_DELAY)
+        if (Input.GetAxis ("Fire1") > 0 && weapon.canShoot)
         {
-			//I would argue we need a better way to store the bullet prefab, allowing upgrades and such to change this.
-			Instantiate (this.bullet, this.transform.position + new Vector3(0.5f, 0, 0), this.transform.rotation);
-			bullettime = 0;
-		}
-	}
+            weapon.Shoot();
+        }
+    }
 
-	void FixedUpdate()
+    void FixedUpdate()
     {
-		rbody.velocity = new Vector2 (xvelocity, yvelocity) * Time.deltaTime * speed;
-	}
+        rbody.velocity = new Vector2 (xvelocity, yvelocity) * Time.deltaTime * speed;
+    }
 }
---
+++ Assets/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject[] bullets;
+    public int bulletType = 0;
+    public float bullettime;
+    public float bullet_delay = 0.1f;
+    public bool canShoot;
+    public float xvelocity;
+    public float yvelocity;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Bullet bullet = this.bullets[bulletType].GetComponent<Bullet> ();
+
+        if (this.bullettime < this.bullet_delay) this.bullettime += Time.deltaTime;
+
+        if (this.bullettime >= this.bullet_delay)
+            canShoot = true;
+        else
+            canShoot = false;
+    }
+
+    public void Shoot()
+    {
+        Bullet bullet = this.bullets[bulletType].GetComponent<Bullet>();
+
+        //I would argue we need a better way to store the bullet prefab, allowing upgrades and such to change this.
+        Instantiate(this.bullets[bulletType], this.transform.position + new Vector3(xvelocity, yvelocity, 0), this.transform.rotation);
+        this.bullettime = 0;
+    }
+}
