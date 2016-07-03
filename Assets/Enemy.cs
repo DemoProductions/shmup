5fd84fcba939f545efc41b5fa13091d3ec0afd44
Adrian Lin <adrianylin@gmail.com>
2016-07-02 19:56:58 -0700

    Can attach a weapon to the Enemy script component

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,21 +3,24 @@
 public class Enemy : MonoBehaviour
 {
     Health health;
-    Weapon weapon;
+    public GameObject weapon;
     Rigidbody2D rbody;
 
     // Use this for initialization
     void Start ()
     {
         health = gameObject.GetComponent<Health> ();
-        weapon = gameObject.GetComponentInChildren<Weapon> ();
+
+        weapon = Instantiate(weapon, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0), Quaternion.identity) as GameObject;
+        weapon.transform.SetParent(this.gameObject.transform);
+
         rbody = gameObject.GetComponent<Rigidbody2D> ();
     }
     
     // Update is called once per frame
     void Update ()
     {
-        weapon.Shoot();
+        weapon.GetComponent<Weapon>().Shoot();
     }
 
     void FixedUpdate()
