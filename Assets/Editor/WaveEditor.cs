0d613136f02a31ddcdeb427cae1886ba3ecc9494
flip40 <john@ludeman.com>
2016-07-06 16:23:36 -0700

    fix curly brace for niceties

--- Assets/Editor/WaveEditor.cs
+++ Assets/Editor/WaveEditor.cs
@@ -4,7 +4,8 @@ using UnityEditor;
 [CustomEditor(typeof(Wave))]
 public class WaveHandleEditor : Editor
 {
-	public override void OnInspectorGUI () {
+	public override void OnInspectorGUI ()
+	{
 		serializedObject.Update();
 		WaveList.Show(serializedObject.FindProperty("spawnables"));
 		serializedObject.ApplyModifiedProperties();
