1726df51e54847bb6480d3faca6a29199df47cee
Adrian Lin <adrianylin@gmail.com>
2016-07-07 23:05:01 -0700

    projectiles do not harm those that are on the same team

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -7,11 +7,13 @@ public class Projectile : MonoBehaviour
 	public int damage = 10;
 
     Rigidbody2D rbody;
+    Team team;
 
     // Use this for initialization
     void Start()
     {
         rbody = gameObject.GetComponent<Rigidbody2D>();
+        team = gameObject.GetComponent<Team>();
     }
 
     // Update is called once per frame
@@ -28,12 +30,13 @@ public class Projectile : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         Health health = other.gameObject.GetComponent<Health>();
+        Team otherTeam = other.GetComponent<Team>();
 
-        // check whether the colliding object can be damaged
-        if (health)
+        // check whether the colliding object can be damaged and can not harm objects that are on the same team
+        if (health && otherTeam.GetTeam() != this.team.GetTeam())
         {
             health.Damage(damage);
             Destroy(this.gameObject);
-        }   
+        }
     }
 }
