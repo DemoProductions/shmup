4bb71c39780dfffe81e46476a618a6a28af6ce2e
Adrian Lin <adrianylin@gmail.com>
2016-07-16 14:19:08 -0700

    able to handle health increasing. HealthBar images anchored to the top left of the screen

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -3,23 +3,24 @@
 [RequireComponent(typeof(Health))]
 public class HealthBar : MonoBehaviour
 {
-    static int healthpointOffset = 125;
+    static int healthpointOffset = 25;
 
     public GameObject healthbarCanvasPrefab;
+
+    // healthpoint images are anchored to the top left of the screen
     public GameObject healthpointImageFullPrefab;
     public GameObject healthpointImageEmptyPrefab;
-    public Vector3 pos = new Vector3 (0, 0, 0);
 
     GameObject healthbarCanvas;
     GameObject[] healthpointImages;
-    int healthpointCursor; // keeps track of how much health the healthbar is showing
+    int healthpointCursor; // keeps track of how much health healthbarCanvas is showing
 
 	// Use this for initialization
 	void Start ()
     {
         if (healthbarCanvasPrefab)
         {
-            healthbarCanvas = Instantiate (healthbarCanvasPrefab, pos, Quaternion.identity) as GameObject;
+            healthbarCanvas = Instantiate (healthbarCanvasPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
 
             // initialize healthpointImages array and healthpointCursor
             Health health = gameObject.GetComponent<Health> ();
@@ -29,7 +30,7 @@ public class HealthBar : MonoBehaviour
             // instantiate healthpoint images and add them to the healthpoints array
             for (int i = 0; i < health.hp; i++)
             {
-                GameObject healthpointImage = Instantiate (healthpointImageFullPrefab, pos + new Vector3(i * healthpointOffset, pos.y, 0), Quaternion.identity) as GameObject;
+                GameObject healthpointImage = Instantiate (healthpointImageFullPrefab, new Vector3(i * healthpointOffset, 0, 0), Quaternion.identity) as GameObject;
                 healthpointImage.transform.SetParent (healthbarCanvas.transform);
                 healthpointImages[i] = healthpointImage;
             }
@@ -42,16 +43,29 @@ public class HealthBar : MonoBehaviour
         Health health = gameObject.GetComponent<Health> ();
 
         // healthpointsCursor to match the player's health
-        // TODO: current assumption is that health can only go down, best to also account for cases when the health goes up.
-        // also should have a loop for this instead of relying on Update()
         if (health.hp - 1 < healthpointCursor)
         {
-            healthpointCursor = healthpointCursor - 1;
-            GameObject healthpointImage = healthpointImages[healthpointCursor + 1];
-            GameObject newHealthpointImage = Instantiate (healthpointImageEmptyPrefab, pos + new Vector3(healthpointImage.transform.position.x, healthpointImage.transform.position.y, 0), Quaternion.identity) as GameObject;
-            newHealthpointImage.transform.SetParent (healthbarCanvas.transform);
-            healthpointImages[healthpointCursor + 1] = healthpointImage;
-            Destroy (healthpointImage.gameObject);
+            while (health.hp - 1 != healthpointCursor)
+            {
+                healthpointCursor = healthpointCursor - 1;
+                GameObject healthpointImage = healthpointImages[healthpointCursor + 1];
+                GameObject newHealthpointImage = Instantiate(healthpointImageEmptyPrefab, new Vector3(healthpointImage.transform.position.x, healthpointImage.transform.position.y, 0), Quaternion.identity) as GameObject;
+                newHealthpointImage.transform.SetParent(healthbarCanvas.transform);
+                healthpointImages[healthpointCursor + 1] = newHealthpointImage;
+                Destroy(healthpointImage.gameObject);
+            }
+        }
+        else if (health.hp - 1 > healthpointCursor)
+        {
+            while (health.hp - 1 != healthpointCursor)
+            {
+                healthpointCursor = healthpointCursor + 1;
+                GameObject healthpointImage = healthpointImages[healthpointCursor];
+                GameObject newHealthpointImage = Instantiate(healthpointImageFullPrefab, new Vector3(healthpointImage.transform.position.x, healthpointImage.transform.position.y, 0), Quaternion.identity) as GameObject;
+                newHealthpointImage.transform.SetParent(healthbarCanvas.transform);
+                healthpointImages[healthpointCursor] = newHealthpointImage;
+                Destroy(healthpointImage.gameObject);
+            }
         }
 	}
 }
