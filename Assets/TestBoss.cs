dd6809411c817bb08b50042d53bfecaa79b431e4
Adrian Lin <adrianylin@gmail.com>
2016-09-08 21:07:23 -0700

    Can transition between boss states with a timer; add TestBoss

---
+++ Assets/TestBoss.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBoss : MonoBehaviour
+{
+
+	Health health;
+	public Weapon weapon;
+	Rigidbody2D rbody;
+	TestBossAI testBossAI;
+	SpriteRenderer spriterenderer;
+
+	bool spawned = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		health = gameObject.GetComponent<Health> ();
+
+		if (weapon)
+		{
+			weapon = Instantiate (weapon, new Vector3 (this.transform.position.x, this.transform.position.y, 0), this.transform.rotation) as Weapon;
+			weapon.transform.SetParent (this.transform);
+			weapon.enabled = false;
+		}
+
+		rbody = this.GetComponent<Rigidbody2D> ();
+
+		testBossAI = this.GetComponent<TestBossAI> ();
+		testBossAI.enabled = false;
+
+		spriterenderer = this.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!spawned && spriterenderer.IsVisibleFrom (Camera.main))
+		{
+			Spawn ();
+		}
+	}
+
+	void Spawn ()
+	{
+		testBossAI.enabled = true;
+		weapon.enabled = true;
+	}
+}
---
+++ Assets/TestBossAI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBossAI : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
